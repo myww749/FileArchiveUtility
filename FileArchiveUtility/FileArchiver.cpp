ff3a69d54e2a2dcabc26c4917b8ca86f6960bf03
brq <brq@10.64.187.126>
2015-09-14 11:31:13 +1000

    FileArchiver init

---
+++ FileArchiveUtility/FileArchiver.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   FileArchiver.cpp
+ * Author: brq
+ * 
+ * Created on 14 September 2015, 11:30 AM
+ */
+
+#include "FileArchiver.h"
+
+FileArchiver::FileArchiver() {
+}
+
+FileArchiver::FileArchiver(const FileArchiver& orig) {
+}
+
+FileArchiver::~FileArchiver() {
+}
+
---
+++ FileArchiveUtility/FileArchiver.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   FileArchiver.h
+ * Author: brq
+ *
+ * Created on 14 September 2015, 11:30 AM
+ */
+
+#ifndef FILEARCHIVER_H
+#define	FILEARCHIVER_H
+
+class FileArchiver {
+public:
+    FileArchiver();
+    FileArchiver(const FileArchiver& orig);
+    virtual ~FileArchiver();
+private:
+
+};
+
+#endif	/* FILEARCHIVER_H */
+
